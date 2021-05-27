3e0db95269d8c57594832619df056d414e02a911
sahrhayat <84861314+sahrhayat@users.noreply.github.com>
2021-05-27 10:19:31 -0400

    Update test_hello_world.py

--- test_hello_world.py
+++ test_hello_world.py
@@ -25,6 +25,15 @@ class TestHelloWorld(unittest.TestCase):
         greeting = self.app.get('/test')
         self.assertIn('Welcome to Circleci', greeting.data.decode('utf-8'))
         
+    def test_third_status_code(self):
+        response = self.app.get('/page3')
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_third_greeting_message(self):
+        greeting = self.app.get('/page3')
+        self.assertIn('Test page', greeting.data.decode('utf-8'))
+        
 
 if __name__ == '__main__':
     unittest.main()
